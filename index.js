0d74e42afa4d9c8504026f68187226c1f1dfcc81
Evan You <yyx990803@gmail.com>
2015-02-23 18:16:25 -0500

    fix styling

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 var compiler = require('vue-component-compiler')
 
 module.exports = function(content) {
-    this.cacheable && this.cacheable()
-    var cb = this.async()
-    compiler.compile(content, this.resourcePath, function(err, result) {
-        if(err) return cb(err)
-        cb(null, result)
-    });
+  this.cacheable && this.cacheable()
+  var cb = this.async()
+  compiler.compile(content, this.resourcePath, function(err, result) {
+    if(err) return cb(err)
+    cb(null, result)
+  })
 }
