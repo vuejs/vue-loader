be9caaa4b1ee31b5e91004ad4b7c13bc2513abd7
Evan You <yyx990803@gmail.com>
2015-04-23 22:13:13 -0400

    1.1.5 - add support for tracking src import dependency

--- index.js
+++ index.js
@@ -3,10 +3,22 @@ var compiler = require('vue-component-compiler')
 compiler.loadConfig()
 
 module.exports = function(content) {
-  this.cacheable && this.cacheable()
-  var cb = this.async()
-  compiler.compile(content, this.resourcePath, function(err, result) {
+  var ctx = this
+  var cb = ctx.async()
+  ctx.cacheable && ctx.cacheable()
+
+  var deps = {}
+  compiler.on('dependency', addDep)
+  function addDep (filePath) {
+    if (!deps[filePath]) {
+      deps[filePath] = true
+      ctx.addDependency(filePath)
+    }
+  }
+
+  compiler.compile(content, ctx.resourcePath, function(err, result) {
     if(err) return cb(err)
+    compiler.removeListener('dependency', addDep)
     cb(null, result)
   })
 }
