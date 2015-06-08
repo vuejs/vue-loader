89d7d515025a8dd5d72285796f39dfafd91a86c0
Sjoerd Visscher <sjoerd@w3future.com>
2015-06-08 13:39:55 +0200

    v0.0.5 Parse with htmlparser2
    Fixes #1

--- parser.js
+++ parser.js
@@ -1,5 +1,5 @@
 var parse5 = require('parse5');
-var parser = new parse5.Parser(null, { locationInfo: true });
+var parser = new parse5.Parser(parse5.TreeAdapters.htmlparser2, { locationInfo: true });
 var serializer = new parse5.TreeSerializer();
 var SourceNode = require("source-map").SourceNode;
 var loaderUtils = require("loader-utils");
@@ -26,26 +26,26 @@ module.exports = function (content) {
   }
 
   var fragment = parser.parseFragment(content);
-  fragment.childNodes.forEach(function (node) {
-    var type = node.nodeName;
-    if (type == '#text')
+  fragment.children.forEach(function (node) {
+    if (node.attribs && node.attribs.src) {
+      output.includes.push(node.attribs.src)
       return;
-    if (checkSrc(node, output.includes))
-      return;
-
-    var lang = checkLang(node) || '';
-
-    // Work around changes in parse5 >= 1.2.0
-    var childNode = node.childNodes[0];
-    if (childNode && childNode.nodeName === '#document-fragment') {
-      node = childNode;
     }
 
-    if (!node.childNodes.length)
+    if (!node.children || !node.children.length)
       return;
 
-    var start = node.childNodes[0].__location.start;
-    var end = node.childNodes[node.childNodes.length - 1].__location.end;
+    var lang = (node.attribs && node.attribs.lang) || '';
+    var type = node.name;
+    if (!output[type])
+      return;
+
+    // Work around changes in parse5 >= 1.2.0
+    if (node.children[0].type === 'root')
+      node = node.children[0];
+
+    var start = node.children[0].__location.start;
+    var end = node.children[node.children.length - 1].__location.end;
     var lines = content.substring(start, end).split('\n');
     var startPos = pos(start);
     var sourceNodes = lines.map(function (line, i) {
@@ -64,30 +64,4 @@ module.exports = function (content) {
   }
 
   cb(null, 'module.exports = ' + JSON.stringify(output));
-}
-
-function checkLang (node) {
-  if (node.attrs) {
-    var i = node.attrs.length;
-    while (i--) {
-      var attr = node.attrs[i];
-      if (attr.name === 'lang') {
-        return attr.value;
-      }
-    }
-  }
-}
-
-function checkSrc (node, arr) {
-  if (node.attrs) {
-    var i = node.attrs.length;
-    while (i--) {
-      var attr = node.attrs[i];
-      if (attr.name === 'src') {
-        arr.push(attr.value);
-        return true;
-      }
-    }
-  }
-  return false
 }
