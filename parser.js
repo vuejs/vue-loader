95dd6262c06a824ebec36307fd2aa7666318574d
Evan You <yyx990803@gmail.com>
2015-06-14 18:41:36 -0400

    some formatting and comments

--- parser.js
+++ parser.js
@@ -1,22 +1,22 @@
-var parse5 = require('parse5');
-var parser = new parse5.Parser(parse5.TreeAdapters.htmlparser2, { locationInfo: true });
-var serializer = new parse5.TreeSerializer();
-var SourceNode = require("source-map").SourceNode;
-var loaderUtils = require("loader-utils");
+var parse5 = require('parse5')
+var parser = new parse5.Parser(parse5.TreeAdapters.htmlparser2, { locationInfo: true })
+var serializer = new parse5.TreeSerializer()
+var SourceNode = require("source-map").SourceNode
+var loaderUtils = require("loader-utils")
 
 module.exports = function (content) {
-  this.cacheable();
-  var cb = this.async();
-  var vueRequest = loaderUtils.getRemainingRequest(this);
-  var request = loaderUtils.getCurrentRequest(this);
+  this.cacheable()
+  var cb = this.async()
+  var vueRequest = loaderUtils.getRemainingRequest(this)
+  var request = loaderUtils.getCurrentRequest(this)
 
-  var languages = {};
+  var languages = {}
   var output = {
     template: {},
     style: {},
     script: {},
     includes: []
-  };
+  }
 
   function pos(offset) {
     return {
@@ -25,43 +25,43 @@ module.exports = function (content) {
     }
   }
 
-  var fragment = parser.parseFragment(content);
+  var fragment = parser.parseFragment(content)
   fragment.children.forEach(function (node) {
     if (node.attribs && node.attribs.src) {
       output.includes.push(node.attribs.src)
-      return;
+      return
     }
 
     if (!node.children || !node.children.length)
-      return;
+      return
 
-    var lang = (node.attribs && node.attribs.lang) || '';
-    var type = node.name;
+    var lang = (node.attribs && node.attribs.lang) || ''
+    var type = node.name
     if (!output[type])
-      return;
+      return
 
     // Work around changes in parse5 >= 1.2.0
     if (node.children[0].type === 'root')
-      node = node.children[0];
+      node = node.children[0]
 
-    var start = node.children[0].__location.start;
-    var end = node.children[node.children.length - 1].__location.end;
-    var lines = content.substring(start, end).split('\n');
-    var startPos = pos(start);
+    var start = node.children[0].__location.start
+    var end = node.children[node.children.length - 1].__location.end
+    var lines = content.substring(start, end).split('\n')
+    var startPos = pos(start)
     var sourceNodes = lines.map(function (line, i) {
-      return new SourceNode(startPos.line + i, i ? 0 : startPos.col, vueRequest, line + '\n');
-    });
+      return new SourceNode(startPos.line + i, i ? 0 : startPos.col, vueRequest, line + '\n')
+    })
     output[type][lang] = (output[type][lang] || []).concat(sourceNodes)
-  });
+  })
 
   for (var type in output) {
     for (var lang in output[type]) {
-      var sourceNodes = output[type][lang];
+      var sourceNodes = output[type][lang]
       output[type][lang] = new SourceNode(1, 1, vueRequest, sourceNodes).toStringWithSourceMap({
         file: request
       })
     }
   }
 
-  cb(null, 'module.exports = ' + JSON.stringify(output));
+  cb(null, 'module.exports = ' + JSON.stringify(output))
 }
--- selector.js
+++ selector.js
@@ -1,14 +1,14 @@
 module.exports = function () {
-  this.cacheable();
-  var cb = this.async();
-  var path = this.query.substr(1).split('/');
+  this.cacheable()
+  var cb = this.async()
+  var path = this.query.substr(1).split('/')
 
-  var me = this;
-  var url = "!!" + require.resolve("./parser.js") + "!" + this.resource;
+  var self = this
+  var url = "!!" + require.resolve("./parser.js") + "!" + this.resource
   this.loadModule(url, function(err, source) {
-    if (err) return cb(err);
-    var parts = me.exec(source, url);
-    var part = parts[path[0]][path[1]||''];
-    cb(null, part.code, part.map);
+    if (err) return cb(err)
+    var parts = self.exec(source, url)
+    var part = parts[path[0]][path[1]||'']
+    cb(null, part.code, part.map)
   })
 }
