5c0d4919bcd3b1a276a8b7093d439512c469288c
Evan You <yyx990803@gmail.com>
2016-01-01 23:54:20 -0500

    fix script line numbers for babel/eslint and eslint indentation warning

--- test/test.js
+++ test/test.js
@@ -209,7 +209,7 @@ describe('vue-loader', function () {
     }), function (err) {
       expect(err).to.be.null
       getFile('test.output.css', function (data) {
-        expect(data).to.contain('h1 {\n  color: #f00;\n}\nh2 {\n  color: green;\n}')
+        expect(data).to.contain('h1 {\n  color: #f00;\n}\n\nh2 {\n  color: green;\n}')
         done()
       })
     })
