3cb9be86ed79d4f84cca299ed98fd0157e36eb50
kazuya kawaguchi <kawakazu80@gmail.com>
2017-01-17 11:47:53 +0900

    add fixture

---
+++ test/fixtures/transform.vue
@@ -0,0 +1,12 @@
+<template>
+<div>
+  <img src="./logo.png">
+  <svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink= "http://www.w3.org/1999/xlink">
+    <image xlink:href="./logo.png" />
+  </svg>
+</div>
+</template>
+
+<style>
+html { background-image: url(./logo.png); }
+</style>
