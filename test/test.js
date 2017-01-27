def3703b0e0c3f6c45a98a23514bd905253e0064
Evan You <yyx990803@gmail.com>
2017-01-27 09:41:51 -0500

    always use absolute path for lib files (close #525)

--- test/test.js
+++ test/test.js
@@ -1,15 +1,15 @@
 process.env.VUE_LOADER_TEST = true
 
 var path = require('path')
+var jsdom = require('jsdom')
 var webpack = require('webpack')
 var MemoryFS = require('memory-fs')
-var jsdom = require('jsdom')
 var expect = require('chai').expect
 var genId = require('../lib/gen-id')
-var SourceMapConsumer = require('source-map').SourceMapConsumer
-var ExtractTextPlugin = require("extract-text-webpack-plugin")
 var compiler = require('../lib/template-compiler')
 var normalizeNewline = require('normalize-newline')
+var ExtractTextPlugin = require("extract-text-webpack-plugin")
+var SourceMapConsumer = require('source-map').SourceMapConsumer
 
 var loaderPath = 'expose-loader?vueModule!' + path.resolve(__dirname, '../index.js')
 var mfs = new MemoryFS()
