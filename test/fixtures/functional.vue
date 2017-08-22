c8a016aa3a351233ee9dc609d3c690864a3978e4
Blake Newman <blake.newman@askattest.com>
2017-08-22 17:29:16 +0100

    feat: (wip) support for functional template compilation

---
+++ test/fixtures/functional.vue
@@ -0,0 +1,22 @@
+<template functional>
+  <div>
+    <h2 class="red">{{ props.msg }}</h2>
+    <slot></slot>
+    <slot name="slot2"></slot>
+    <slot :msg="props.msg" name="scoped"></slot>
+    <div>Some <span>text</span></div>
+    <div v-if="false">Not exist</div>
+    <div class="clickable" @click="parent.fn"></div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    msg: {
+      type: String,
+      default: 'hello'
+    }
+  }
+}
+</script>
