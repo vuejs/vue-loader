d31d0976ab78a534eb2816c83590779822e87c68
André Hänsel <haensel@creations.de>
2017-09-19 18:39:29 +0200

    Further fixes for whitespace handling
    Also added a test for srcset.

--- test/fixtures/transform.vue
+++ test/fixtures/transform.vue
@@ -4,6 +4,14 @@
   <svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink= "http://www.w3.org/1999/xlink">
     <image xlink:href="./logo.png" />
   </svg>
+  <img src="./logo.png" srcset="./logo.png 2x">
+  <img src="./logo.png" srcset="./logo.png 2x, ./logo.png 3x">
+  <img
+    src="./logo.png"
+    srcset="
+      ./logo.png 2x,
+      ./logo.png 3x
+  ">
 </div>
 </template>
 
