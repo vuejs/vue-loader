7208885a1ed24f5f1b997971b413492caa0e6bfa
Evan You <yyx990803@gmail.com>
2018-03-23 13:19:22 -0400

    fix: support test-less oneOf rules

--- test/edgeCases.spec.js
+++ test/edgeCases.spec.js
@@ -5,6 +5,23 @@ const {
 
 const normalizeNewline = require('normalize-newline')
 
+const assertComponent = ({ window, module }, done) => {
+  const vnode = mockRender(module, {
+    msg: 'hi'
+  })
+
+  // <h2 class="red">{{msg}}</h2>
+  expect(vnode.tag).toBe('h2')
+  expect(vnode.data.staticClass).toBe('red')
+  expect(vnode.children[0].text).toBe('hi')
+
+  expect(module.data().msg).toContain('Hello from Component A!')
+  let style = window.document.querySelector('style').textContent
+  style = normalizeNewline(style)
+  expect(style).toContain('comp-a h2 {\n  color: #f00;\n}')
+  done()
+}
+
 test('vue rule with include', done => {
   mockBundleAndRun({
     entry: 'basic.vue',
@@ -15,20 +32,27 @@ test('vue rule with include', done => {
         loader: 'vue-loader'
       }
     }
-  }, ({ window, module, rawModule }) => {
-    const vnode = mockRender(module, {
-      msg: 'hi'
-    })
+  }, res => assertComponent(res, done))
+})
 
-    // <h2 class="red">{{msg}}</h2>
-    expect(vnode.tag).toBe('h2')
-    expect(vnode.data.staticClass).toBe('red')
-    expect(vnode.children[0].text).toBe('hi')
-
-    expect(module.data().msg).toContain('Hello from Component A!')
-    let style = window.document.querySelector('style').textContent
-    style = normalizeNewline(style)
-    expect(style).toContain('comp-a h2 {\n  color: #f00;\n}')
-    done()
-  })
+test('test-less oneOf rules', done => {
+  mockBundleAndRun({
+    entry: 'basic.vue',
+    modify: config => {
+      config.module.rules = [
+        {
+          test: /\.vue$/,
+          loader: 'vue-loader'
+        },
+        {
+          oneOf: [
+            {
+              test: /\.css$/,
+              use: ['vue-style-loader', 'css-loader']
+            }
+          ]
+        }
+      ]
+    }
+  }, res => assertComponent(res, done))
 })
