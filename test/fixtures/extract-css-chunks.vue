c444ab6121248043978837247567bf5296abcf0d
JK <jingkai.zhao@foxmail.com>
2019-01-08 14:45:02 +0800

    fix: avoid to generate empty css chunk files (#1464)
    * test: add failing test
    
    * fix: filter out empty styles before generating code
    
    * feat: improve regex

---
+++ test/fixtures/extract-css-chunks.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <basic></basic>
+    <empty-style></empty-style>
+  </div>
+</template>
+
+<script>
+export default {
+  components: {
+    Basic: () => import(/* webpackChunkName: "basic" */ './basic.vue'),
+    EmptyStyle: () => import(/* webpackChunkName: "empty" */ './empty-style.vue')
+  }
+}
+</script>
+
+<style>
+h1 {
+  color: red;
+}
+</style>
