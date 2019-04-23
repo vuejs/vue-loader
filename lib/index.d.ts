0f50267423dade08f43f0521a1a14ec3a66933c2
kbtzr <1047033+kbtzr@users.noreply.github.com>
2019-04-23 06:18:59 -0300

    types: add loader options declarations

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -1,7 +1,40 @@
 import { Plugin } from 'webpack'
+import {
+  CompilerOptions,
+  parseComponent,
+  compile,
+  compileToFunctions,
+  ssrCompile,
+  ssrCompileToFunctions,
+  generateCodeFrame
+} from 'vue-template-compiler'
 
 declare namespace VueLoader {
   class VueLoaderPlugin extends Plugin {}
+
+  interface VueLoaderTemplateCompiler {
+    parseComponent?: typeof parseComponent
+    compile?: typeof compile
+    compileToFunctions?: typeof compileToFunctions
+    ssrCompile?: typeof ssrCompile
+    ssrCompileToFunctions?: typeof ssrCompileToFunctions
+    generateCodeFrame?: typeof generateCodeFrame
+  }
+
+  interface VueLoaderOptions {
+    transformAssetUrls?: { [tag: string]: string | Array<string> }
+    compiler?: VueLoaderTemplateCompiler
+    compilerOptions?: CompilerOptions
+    transpileOptions?: Object
+    optimizeSSR?: boolean
+    hotReload?: boolean
+    productionMode?: boolean
+    shadowMode?: boolean
+    cacheDirectory?: string
+    cacheIdentifier?: string
+    prettify?: boolean
+    exposeFilename?: boolean
+  }
 }
 
 export = VueLoader
