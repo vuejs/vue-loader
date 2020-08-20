eec3c80545f718021f5ee40a94df9d613a2532ba
Evan You <yyx990803@gmail.com>
2020-08-20 17:52:54 -0400

    build(deps): bump vue and TS versions

--- src/formatError.ts
+++ src/formatError.ts
@@ -1,17 +1,19 @@
 import { generateCodeFrame, CompilerError } from '@vue/compiler-sfc'
 import chalk from 'chalk'
 
-export function formatError(err: CompilerError, source: string, file: string) {
-  if (err.loc) {
-    const loc = `:${err.loc.start.line}:${err.loc.start.column}`
-    const filePath = chalk.gray(`at ${file}${loc}`)
-    const codeframe = generateCodeFrame(
-      source,
-      err.loc.start.offset,
-      err.loc.end.offset
-    )
-    err.message = `\n${chalk.red(
-      `VueCompilerError: ${err.message}`
-    )}\n${filePath}\n${chalk.yellow(codeframe)}\n`
+export function formatError(
+  err: SyntaxError | CompilerError,
+  source: string,
+  file: string
+) {
+  const loc = (err as CompilerError).loc
+  if (!loc) {
+    return
   }
+  const locString = `:${loc.start.line}:${loc.start.column}`
+  const filePath = chalk.gray(`at ${file}${locString}`)
+  const codeframe = generateCodeFrame(source, loc.start.offset, loc.end.offset)
+  err.message = `\n${chalk.red(
+    `VueCompilerError: ${err.message}`
+  )}\n${filePath}\n${chalk.yellow(codeframe)}\n`
 }
