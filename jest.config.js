c070aeb7f411fb1d22ae13e8f1f9b3b6b5529b04
Haoqun Jiang <haoqunjiang@gmail.com>
2020-11-04 21:03:37 +0800

    chore: use env variable to choose the webpack version to test

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,24 @@
-module.exports = {
+const config = {
   preset: 'ts-jest',
   testTimeout: 10000,
   testEnvironment: 'node',
   testPathIgnorePatterns: ['<rootDir>/dist/', '<rootDir>/node_modules/'],
+}
+
+if (process.env.WEBPACK5) {
+  module.exports = {
+    ...config,
+
+    globals: {
+      'ts-jest': {
+        diagnostics: false,
+      },
+    },
+    moduleNameMapper: {
+      '^webpack$': 'webpack5',
+      '^webpack/(.*)': 'webpack5/$1',
+    },
+  }
+} else {
+  module.exports = config
 }
