b391b04ed3335a2d2c9010101d98c2d9977626e8
Evan You <yyx990803@gmail.com>
2021-11-26 14:14:54 +0800

    chore: remove unncessary log

--- src/hotReload.ts
+++ src/hotReload.ts
@@ -11,7 +11,6 @@ if (module.hot) {
   const api = __VUE_HMR_RUNTIME__
   module.hot.accept()
   if (!api.createRecord('${id}', __exports__)) {
-    console.log('reload')
     api.reload('${id}', __exports__)
   }
   ${templateRequest ? genTemplateHotReloadCode(id, templateRequest) : ''}
@@ -22,7 +21,6 @@ if (module.hot) {
 function genTemplateHotReloadCode(id: string, request: string) {
   return `
   module.hot.accept(${request}, () => {
-    console.log('re-render')
     api.rerender('${id}', render)
   })
 `
