55de28c7dbe1a222d282555faa2286127edb2128
Evan You <yyx990803@gmail.com>
2022-06-15 10:15:48 +0800

    feat: use script-analyzed bindings when compiling template

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,8 @@
 module.exports = {
   root: true,
-  extends: ['plugin:vue-libs/recommended']
+  extends: ['plugin:vue-libs/recommended'],
+  rules: {
+    indent: 'off',
+    'space-before-function-paren': 'off'
+  }
 }
